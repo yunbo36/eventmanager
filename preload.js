bf16760055215db7de1c3d693534ef9598211d3e
yunbo36 <sic.miura@gmail.com>
2024-10-14 18:51:36 +0900

    Sync with local changes

--- preload.js
+++
@@ -1,8 +0,0 @@
-import { contextBridge, ipcRenderer } from 'electron';
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  openFolderDialog: () => ipcRenderer.invoke('open-folder-dialog'),
-  readExcelFile: (filePath) => ipcRenderer.invoke('read-excel-file', filePath),
-  saveFile: (content, defaultPath) => ipcRenderer.invoke('save-file', content, defaultPath),
-  openFolder: (path) => ipcRenderer.invoke('open-folder', path)
-});
